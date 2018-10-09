ea69327c4e5f22651b5aea88cd7654962d53deff
Thomas_main <t.f.m.lacroix@gmail.com>
2018-10-09 23:19:40 +0200

    added more css/js/html

--- app.js
+++ app.js
@@ -8,14 +8,19 @@ menuToggle.addEventListener('click', () =>
 {
     menu.classList.toggle('open')
     container.classList.toggle('small')
+    menuToggle.classList.toggle('move')
 })
+for(let = i; i < elements.length; i++){
+  elements[i].addEventListener('click', () =>
+  {
+    
+  })
+}
 window.addEventListener('mousemove', (_event) =>
 {
-  const x = - (window.innerWidth/2 -_event.clientX)/300
-  const y = (450 - _event.clientY)/100
-  console.log(((window.innerWidth/2 -_event.clientX)/100),450 - _event.clientY)
-  elements[0].style.transform = "rotateY("+x+"deg) rotateX("+y+"deg)";
-  elements[1].style.transform = "rotateY("+x+"deg) rotateX("+y+"deg)";
-  elements[2].style.transform = "rotateY("+x+"deg) rotateX("+y+"deg)";
-  elements[3].style.transform = "rotateY("+x+"deg) rotateX("+y+"deg)";
+  const x = (window.innerWidth/2 -_event.clientX)/300
+  const y = (window.innerHeight/2 - _event.clientY)/100
+  for(let i = 0; i < elements.length; i++){
+      elements[i].style.transform = "rotateY("+x+"deg) rotateX("+y+"deg)";
+  }
 })
