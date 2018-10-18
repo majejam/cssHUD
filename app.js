25595c3da6ac29993d03a7fef9bcd9c4326d64d1
Thomas <t.f.m.lacroix@gmail.com>
2018-10-18 15:18:53 +0200

    added bigger menu

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const elements = document.querySelectorAll('.hud-piece')
 const menuToggle = document.querySelector('.button-toggle-menu')
 const menu = document.querySelector('.menu')
 const container = document.querySelector('.hud-big-container')
+const loaderContainer = document.querySelector('.loader-container')
+const cubeElements = document.querySelectorAll('.cube-3d')
 const width = 400;
 const heigth = 200;
 menuToggle.addEventListener('click', () =>
@@ -10,7 +12,12 @@ menuToggle.addEventListener('click', () =>
     container.classList.toggle('small')
     menuToggle.classList.toggle('move')
 })
-
+elements[1].addEventListener('click', () =>
+{
+    cubeElements[0].classList.toggle('changePlus')
+    cubeElements[1].classList.toggle('changeMinus')
+})
+setTimeout(function(){loaderContainer.style.zIndex = '-15'; }, 1000);
 window.addEventListener('mousemove', (_event) =>
 {
   const x = (window.innerWidth/2 -_event.clientX)/300
