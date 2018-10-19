13b3d246e37db9c8744c35f29509e115637a1388
Thomas_main <t.f.m.lacroix@gmail.com>
2018-10-19 22:02:01 +0200

    added wave effect, menu interaction, and more

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const menu = document.querySelector('.menu')
 const container = document.querySelector('.hud-big-container')
 const loaderContainer = document.querySelector('.loader-container')
 const cubeElements = document.querySelectorAll('.cube-3d')
-const rippleElement = document.querySelector('.ripple-style')
+const rippleElements = document.querySelectorAll('.ripple-style')
+const rippleContainer = document.querySelector('.ripple-container')
+const cubeContainer = document.querySelector('.cube-container')
+const wavesHoverElements = document.querySelectorAll('.single-wave-container')
+const wavesElements = document.querySelectorAll('.wave-style')
+console.log(wavesElements)
 const width = 400;
 const heigth = 200;
+for(let i = 0; i < wavesElements.length; i++){
+  wavesElements[i].style.height = "10px";
+}
 menuToggle.addEventListener('click', () =>
 {
     menu.classList.toggle('open')
@@ -19,14 +27,38 @@ menuToggle.addEventListener('click', () =>
     }
 
 })
+function wavesOnHover(wavesHoverElements, wavesElements){
+  for(let i = 0; i < wavesHoverElements.length; i++){
+    wavesHoverElements[i].addEventListener("mouseover", () =>
+    {
+      if(wavesElements[i].style.height == "10px"){
+        wavesElements[i].style.height = "100px";
+      }else{
+        wavesElements[i].style.height = "10px";
+      }
+    });
+  }
+}
+
+wavesOnHover(wavesHoverElements, wavesElements);
 elements[1].addEventListener('click', () =>
 {
     cubeElements[0].classList.toggle('changePlus')
     cubeElements[1].classList.toggle('changeMinus')
+    cubeContainer.style.visibility = "visible"
+    rippleContainer.style.visibility = "hidden"
+})
+elements[2].addEventListener('click', () =>
+{
+  for(let i = 0; i < wavesElements.length; i++){
+    wavesElements[i].style.height = "10px";
+    wavesElements[i].classList.toggle('wave-animation')
+  }
 })
 elements[3].addEventListener('click', () =>
 {
-  rippleEffect(rippleElement)
+    rippleContainer.style.visibility = "visible"
+    cubeContainer.style.visibility = "hidden"
 })
 setTimeout(function(){loaderContainer.style.zIndex = '-15'; }, 1000);
 window.addEventListener('mousemove', (_event) =>
@@ -37,19 +69,3 @@ window.addEventListener('mousemove', (_event) =>
       elements[i].style.transform = "rotateY("+x+"deg) rotateX("+y+"deg)";
   }
 })
-
-function rippleEffect(rippleElement){
-  window.addEventListener('click', () =>
-  {
-    let posx = event.clientX;
-    let posy = event.clientY;
-    rippleElement.classList.toggle('ripple-style-after')
-    rippleElement.style.top = `${posy}px`
-    rippleElement.style.left = `${posx}px`
-    setTimeout(function(){
-      if(rippleElement.classList.contains('ripple-style-after')){
-        rippleElement.classList.remove('ripple-style-after')
-      }
-    }, 1000);
-  })
-}
