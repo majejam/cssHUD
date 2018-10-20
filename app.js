89a704f43051128bd9805305dfeda03fd5377bba
Thomas <t.f.m.lacroix@gmail.com>
2018-10-20 23:10:16 +0200

    added buttons interraction with the menu

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const wavesHoverElements = document.querySelectorAll('.single-wave-container')
 const wavesElements = document.querySelectorAll('.wave-style')
 const rainContainer = document.querySelector('.rain-container')
 const optionCubeButtons = document.querySelectorAll('.option-cube-selection')
+const optionRippleSelection = document.querySelectorAll('.option-ripple-selection')
+const optionRainSelection = document.querySelectorAll('.option-rain-selection')
+const optionWaveSelection = document.querySelectorAll('.option-wave-selection')
 const width = 400;
 const heigth = 200;
 for(let i = 0; i < wavesElements.length; i++){
@@ -27,16 +30,81 @@ for(let i = 0; i < optionCubeButtons.length; i++){
   {
     clearButtons(optionCubeButtons)
     optionCubeButtons[i].classList.toggle('small-option-button-container-selected')
+    if(i == 0){
+      cubeElements[0].classList.remove('changePlus')
+      cubeElements[1].classList.remove('changeMinus')
+    }
+    if(i == 1){
+      cubeElements[0].classList.add('changePlus')
+      cubeElements[1].classList.add('changeMinus')
+    }
   });
 }
 //Option ripple buttons
-for(let i = 0; i < optionCubeButtons.length; i++){
-  optionCubeButtons[i].addEventListener('click', () =>
+for(let i = 0; i < optionRippleSelection.length; i++){
+  optionRippleSelection[i].addEventListener('click', () =>
   {
-    clearButtons(optionCubeButtons)
-    optionCubeButtons[i].classList.toggle('small-option-button-container-selected')
+    clearButtons(optionRippleSelection)
+    optionRippleSelection[i].classList.toggle('small-option-button-container-selected')
+    if(i == 0){
+      rippleContainer.style.width = "200px"
+      rippleContainer.style.height = "200px"
+    }
+    if(i == 1){
+      rippleContainer.style.width = "400px"
+      rippleContainer.style.height = "400px"
+    }
+    if(i == 2){
+      rippleContainer.style.width = "600px"
+      rippleContainer.style.height = "600px"
+    }
+    if(i == 3){
+      rippleContainer.style.width = "1000px"
+      rippleContainer.style.height = "1000px"
+    }
   });
 }
+//Option rain button
+for(let i = 0; i < optionRainSelection.length; i++){
+  optionRainSelection[i].addEventListener('click', () =>
+  {
+    clearButtons(optionRainSelection)
+    optionRainSelection[i].classList.toggle('small-option-button-container-selected')
+    if(i == 0){
+      rainContainer.style.transform = "skewX(0deg)"
+    }
+    if(i == 1){
+      rainContainer.style.transform = "skewX(10deg)"
+    }
+    if(i == 2){
+      rainContainer.style.transform = "skewX(25deg)"
+    }
+    if(i == 3){
+      rainContainer.style.transform = "skewX(45deg)"
+    }
+  });
+}
+//Option wave buttons
+for(let i = 0; i < optionWaveSelection.length; i++){
+  optionWaveSelection[i].addEventListener('click', () =>
+  {
+    clearButtons(optionWaveSelection)
+    optionWaveSelection[i].classList.toggle('small-option-button-container-selected')
+    if(i == 0){
+      for(let i = 0; i < wavesElements.length; i++){
+        wavesElements[i].style.height = '10px';
+        wavesElements[i].classList.add('wave-animation')
+      }
+    }
+    if(i == 1){
+      for(let i = 0; i < wavesElements.length; i++){
+        wavesElements[i].style.height = '10px';
+        wavesElements[i].classList.remove('wave-animation')
+      }
+    }
+  });
+}
+
 
 function clearButtons(elements){
   for(let i = 0; i < elements.length; i++){
@@ -90,8 +158,6 @@ elements[0].addEventListener('click', () =>
 //Cube button
 elements[1].addEventListener('click', () =>
 {
-    cubeElements[0].classList.toggle('changePlus')
-    cubeElements[1].classList.toggle('changeMinus')
     cubeContainer.style.visibility = 'visible'
     rippleContainer.style.visibility = 'hidden'
     waveContainer.style.visibility = 'hidden'
@@ -100,10 +166,6 @@ elements[1].addEventListener('click', () =>
 //Wave button
 elements[2].addEventListener('click', () =>
 {
-  for(let i = 0; i < wavesElements.length; i++){
-    wavesElements[i].style.height = '10px';
-    wavesElements[i].classList.toggle('wave-animation')
-  }
   waveContainer.style.visibility = 'visible'
   cubeContainer.style.visibility = 'hidden'
   rippleContainer.style.visibility = 'hidden'
